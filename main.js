adc55dce70a356e081d887c5ea79ec66095d18fb
Qodesmith <theqodesmith@gmail.com>
2023-08-07 08:19:56 -0400

    Using node-fetch & cheerio instead - so fast

--- main.js
+++ main.js
@@ -1,38 +1,25 @@
-import puppeteer from 'puppeteer'
+import fetch from 'node-fetch'
+import * as cheerio from 'cheerio'
 
-const browser = await puppeteer.launch({headless: false})
-const page = await browser.newPage()
-
-await page.goto(
+// The hashtag at the end of the url loads the correct tab on the page.
+const bibleGatewayUrl =
   'https://www.biblegateway.com/versions/English-Standard-Version-ESV-Bible#booklist'
-)
 
-const otBooks = await page.$$('.ot-book')
+try {
+  // Fetch the Bible book list page.
+  const booksPageResponse = await fetch(bibleGatewayUrl)
+  const booksPageHtml = await booksPageResponse.text()
+  const $ = cheerio.load(booksPageHtml)
 
-for (let i = 0; i < otBooks.length; i++) {
-  const bookEl = otBooks[i]
-  const bookNameEl = await bookEl.$('.book-name')
-
-  if (!bookNameEl) {
-    throw new Error('Bible book name not found')
+  // Get all the old & new testament sections.
+  const otBookSections = $('.ot-book')
+  const ntBookSections = $('.nt-book')
+  const totalBooksFound = otBookSections.length + ntBookSections.length
+  if (totalBooksFound !== 66) {
+    throw new Error(`${totalBooksFound} books found, expected 66`)
   }
-
-  const bookName = await page.evaluate(el => {
-    // Text node type.
-    const textNode = [...el.childNodes].filter(node => node.nodeType === 3)
-    if (textNode.length > 1) {
-      throw new Error('Multiple text nodes found for Bible book title')
-    }
-
-    return textNode[0].textContent.trim()
-  }, bookNameEl)
-
-  const chapterLinks = await bookEl.$$('.chapters a')
-  const chapterCount = chapterLinks.length
-
-  console.log(bookName, chapterCount)
+} catch (e) {
+  console.error(e)
+} finally {
+  process.exit()
 }
-
-// const ntBooks = await page.$$('.nt-book')
-
-await page.close()
