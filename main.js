c6cca102f8904060c8a4533d78a755b9490bf1d5
Qodesmith <theqodesmith@gmail.com>
2023-08-07 09:23:34 -0400

    Getting book name and chapter urls

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ import fetch from 'node-fetch'
 import * as cheerio from 'cheerio'
 
 // The hashtag at the end of the url loads the correct tab on the page.
-const bibleGatewayUrl =
+const bibleGatewayUrl = new URL(
   'https://www.biblegateway.com/versions/English-Standard-Version-ESV-Bible#booklist'
+)
+const originUrl = bibleGatewayUrl.origin
 
 try {
   // Fetch the Bible book list page.
@@ -14,12 +16,63 @@ try {
   const $ = cheerio.load(booksPageHtml)
 
   // Get all the old & new testament sections.
-  const otBookSections = $('.ot-book')
-  const ntBookSections = $('.nt-book')
-  const totalBooksFound = otBookSections.length + ntBookSections.length
+  const $otBookSections = $('.ot-book')
+  const $ntBookSections = $('.nt-book')
+  const totalBooksFound = $otBookSections.length + $ntBookSections.length
   if (totalBooksFound !== 66) {
     throw new Error(`${totalBooksFound} books found, expected 66`)
   }
+
+  const genesisUrls = getBookUrls($, $otBookSections.first())
+  console.log(genesisUrls)
 } catch (e) {
   console.error(e)
+}
+
+/**
+ * @param {cheerio.CheerioAPI} $
+ * @param {cheerio.Cheerio<cheerio.Element>} $sectionEl
+ */
+function getBookName($, $sectionEl) {
+  const $bookNameEl = $sectionEl.find('.book-name')
+  const {childNodes} = $bookNameEl[0]
+  const $textNode = $(childNodes.find(childNode => childNode.nodeType === 3))
+  const bookName = $textNode.text().trim()
+
+  if (!bookName.length) {
+    throw new Error('Error with getting book name text')
+  }
+
+  return bookName
+}
+
+/**
+ * @param {cheerio.CheerioAPI} $
+ * @param {cheerio.Cheerio<cheerio.Element>} $sectionEl
+ */
+function getChapterUrls($, $sectionEl) {
+  const expectedCount = Number($sectionEl.find('.num-chapters').text())
+  const $chaptersEl = $sectionEl.find('.chapters')
+  const $links = $chaptersEl.find('a')
+
+  if (expectedCount !== $links.length) {
+    throw new Error('Mismatch of expected chapters')
+  }
+
+  return $links
+    .map((i, el) => {
+      return new URL($(el).attr('href') ?? '', originUrl).toString()
+    })
+    .toArray()
+}
+
+/**
+ * @param {cheerio.CheerioAPI} $
+ * @param {cheerio.Cheerio<cheerio.Element>} $sectionEl
+ */
+function getBookUrls($, $sectionEl) {
+  const bookName = getBookName($, $sectionEl)
+  const chapterUrls = getChapterUrls($, $sectionEl)
+
+  return {[bookName]: chapterUrls}
 }
